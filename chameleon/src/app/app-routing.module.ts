17f05eb3bf2c54f394e72dbc700f23489ac9fcdf
Eduardo Macedo <eduardojcmacedo@gmail.com>
2020-11-30 12:32:59 +0000

    rip

--- chameleon/src/app/app-routing.module.ts
+++ chameleon/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OverviewComponent } from './overview/overview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    children: [
+      {
+        path: '',
+        component: OverviewComponent
+      },
+    ]
+  },
+];
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
 export class AppRoutingModule { }
