c74ee799dfa279d866099d6bbbfe5543c238ec64
Eduardo Macedo <eduardojcmacedo@gmail.com>
2020-11-30 15:56:30 +0000

    added simple login page

--- chameleon/src/app/app-routing.module.ts
+++ chameleon/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { OverviewComponent } from './overview/overview.component';
 
 const routes: Routes = [
@@ -8,6 +9,10 @@ const routes: Routes = [
     children: [
       {
         path: '',
+        component: LoginComponent
+      },
+      {
+        path: 'overview',
         component: OverviewComponent
       },
     ]
