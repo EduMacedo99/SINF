f1b3719094a05846358d871a9c9376191e61f8fd
César Nogueira <github@cesarnogueira.com>
2020-12-06 18:21:18 +0000

    Changed backend file structure

--- backend/lib/index.js
+++ backend/lib/index.js
@@ -1,12 +1,21 @@
-import http from "http";
+"use strict";
 
-const server = http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World\n");
-  })
-  .listen(1337, "127.0.0.1");
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _http = _interopRequireDefault(require("http"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var server = _http["default"].createServer(function (req, res) {
+  res.writeHead(200, {
+    "Content-Type": "text/plain"
+  });
+  res.end("Hello World\n");
+}).listen(1337, "127.0.0.1");
 
 console.log("Server running at http://127.0.0.1:1337/");
-
-export default server;
+var _default = server;
+exports["default"] = _default;
