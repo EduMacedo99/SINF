c15da415a1a45fe06663bb3f8a5d0ac069472412
Nuno Cardoso <up201706162@fe.up.pt>
2020-12-06 12:18:16 +0000

    Menu with routes

--- chameleon/src/app/shared-components/shared-components.component.ts
+++ chameleon/src/app/shared-components/shared-components.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    class: string;
+    type: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/overview', title: 'Overview', class: '', type: 'overview' },
+    { path: '/sales', title: 'Sales', class: '', type: 'sales' },
+    { path: '/purchases', title: 'Purchases', class: '', type: 'purchases' },
+    { path: '/financial', title: 'Financial', class: '', type: 'financial' },
+    { path: '/inventory', title: 'Inventory', class: '', type: 'inventory' },
+];
+
 @Component({
   selector: 'app-shared-components',
   templateUrl: './shared-components.component.html',
@@ -7,9 +22,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SharedComponentsComponent implements OnInit {
 
+  menuItems: any[] = [];
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
   }
 
 }
