23095b8fb4347a48cbfb7c7b3f45fd866e7029c3
Eduardo Macedo <eduardojcmacedo@gmail.com>
2020-12-09 13:01:24 +0000

    validation done

--- backend/src/parser.js
+++ backend/src/parser.js
@@ -6,18 +6,12 @@ import shell from "shelljs";
 
 if (shell.which("xmllint")) {
   shell.echo("Validating XML file " + arg.source);
-  let r = shell.exec(
-    "xmllint --noout --schema SAFTPT1.04_011.xsd " + arg.source
-  );
-  if (r.code !== 0) {
-    shell.echo("XML not valid!");
-  } else {
-    shell.echo("Valid XML!");
-  }
+  let r = shell.exec("xmllint --noout --schema saftpt1.04_01_spec.xsd " + arg.source);
+  
+  if (r.code !== 0) shell.echo("XML not valid!");
+  else shell.echo("Valid XML!");
 
-} else {
-  shell.echo('XML Validation requires "xmllint".');
-}
+} else shell.echo('XML Validation requires "xmllint".');
 
 read(arg.source, (err, buffer) => {
 
