1e1d1347a79662cd6e78996901e342be64881b76
Nuno Cardoso <up201706162@fe.up.pt>
2020-12-12 22:53:32 +0000

    Balance sheet

--- chameleon/src/app/financial/financial.component.ts
+++ chameleon/src/app/financial/financial.component.ts
@@ -7,15 +7,95 @@ import { SaftApiService } from 'src/app/saftApi/saft-api.service';
   styleUrls: ['./financial.component.scss', '../app.component.scss']
 })
 export class FinancialComponent implements OnInit {
+  private assets: any;
+  private liabilities: any;
+  private equity: any;
 
   constructor(private saftApi: SaftApiService) { }
 
   ngOnInit(): void {
     this.saftApi.get('api/financial/balance-sheet').subscribe(
       (data:Object) => {
-        if('assets' in data) console.log(data['assets'])
+        if('assets' in data) 
+          this.assets = data['assets'];
+        if('liabilities' in data)
+          this.liabilities = data['assets'];
+        if('equity' in data)
+          this.equity = data['equity'];
+        console.log(data);
       }
     );
   }
 
+  getCurrentAssets(): Array<any> {
+    if (this.assets!= null && 'current' in this.assets)
+      return this.assets['current'];
+    return Array<any>();
+  }
+
+  getNonCurrentAssets(): Array<any> {
+    if (this.assets!= null && 'nonCurrent' in this.assets)
+      return this.assets['nonCurrent'];
+    return Array<any>();
+  }
+
+  getTotalCurrentAssets(): number {
+    if (this.assets!= null && 'totalCurrent' in this.assets)
+      return this.assets['totalCurrent'];
+    return 0;
+  }
+
+  getTotalNonCurrentAssets(): number {
+    if (this.assets!= null && 'totalNonCurrent' in this.assets)
+      return this.assets['totalNonCurrent'];
+    return 0;
+  }
+
+  getTotalAssets(): number {
+    if (this.assets!= null && 'total' in this.assets)
+      return this.assets['total'];
+    return 0;
+  }
+
+  getAccountsEquity(): Array<any> {
+    if (this.equity!= null && 'accounts' in this.equity)
+      return this.equity['accounts'];
+    return Array<any>();
+  }
+
+  getTotalEquity(): number {
+    if (this.equity!= null && 'total' in this.equity)
+      return this.equity['total'];
+    return 0;
+  }
+
+  getCurrentLiabilities(): Array<any> {
+    if (this.liabilities!= null && 'current' in this.liabilities)
+      return this.liabilities['current'];
+    return Array<any>();
+  }
+
+  getNonCurrentLiabilities(): Array<any> {
+    if (this.liabilities!= null && 'nonCurrent' in this.liabilities)
+      return this.liabilities['nonCurrent'];
+    return Array<any>();
+  }
+
+  getTotalCurrentLiabilities(): number {
+    if (this.liabilities!= null && 'totalCurrent' in this.liabilities)
+      return this.liabilities['totalCurrent'];
+    return 0;
+  }
+
+  getTotalNonCurrentLiabilities(): number {
+    if (this.liabilities!= null && 'totalNonCurrent' in this.liabilities)
+      return this.liabilities['totalNonCurrent'];
+    return 0;
+  }
+
+  getTotalLiabilities(): number {
+    if (this.liabilities!= null && 'total' in this.liabilities)
+      return this.liabilities['total'];
+    return 0;
+  }
 }
