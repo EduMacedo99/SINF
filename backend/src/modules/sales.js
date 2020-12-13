5fd2f30fc6c3e51877d4ecfb6d7f186af166baef
César Nogueira <github@cesarnogueira.com>
2020-12-13 13:16:57 +0000

    Top products route

--- backend/src/modules/sales.js
+++ backend/src/modules/sales.js
@@ -40,7 +40,46 @@ module.exports = (server, db) => {
                 sales[parseInt(invoice.Period, 10) - 1];
         });
 
-        res.json( {sales} );
+        res.json({ sales });
+    });
+
+    server.get('/api/sales/top-products', (req, res) => {
+        let products = {};
+        const validTypes = ['FT', 'FS', 'FR', 'VD'];
+
+        db.SourceDocuments.SalesInvoices.Invoice.forEach(invoice => {
+            const type = invoice.InvoiceType;
+
+            if (!(invoice.Line.length && validTypes.includes(type))) return;
+
+            invoice.Line.forEach(line => {
+                const { ProductCode, UnitPrice, ProductDescription, Quantity } = line;
+                if (Object.prototype.hasOwnProperty.call(products, ProductCode)) {
+                    products[ProductCode].Quantity += parseInt(Quantity, 10);
+                } else {
+                    products[ProductCode] = {
+                        ProductDescription,
+                        UnitPrice: parseFloat(UnitPrice, 10),
+                        Quantity: parseInt(Quantity, 10),
+                    };
+                }
+            });
+        });
+
+        products = Object.keys(products)
+            .sort((a, b) => products[b].Quantity - products[a].Quantity)
+            .map(productCode => ({
+                id: productCode,
+                name: products[productCode].ProductDescription,
+                quantity: products[productCode].Quantity,
+                value: Number(
+                    (
+                        products[productCode].Quantity * products[productCode].UnitPrice
+                    ).toFixed(2),
+                ),
+            }));
+
+        res.json(products);
     });
 
     server.get('/api/sales/top-customers', (req, res) => {
