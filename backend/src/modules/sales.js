ddd34b08096766d7f07b39581e46df9cb8489cdf
César Nogueira <github@cesarnogueira.com>
2020-12-13 12:25:31 +0000

    Small fix

--- backend/src/modules/sales.js
+++ backend/src/modules/sales.js
@@ -1,32 +1,32 @@
 module.exports = (server, dbJasmin) => {
-    server.get('/api/sales/sales-per-location', (req, res) => {
+    server.get('/api/sales/sales-per-city', (req, res) => {
         const sales = dbJasmin.SourceDocuments.SalesInvoices.Invoice;
         const validTypes = ['FT', 'FS', 'FR', 'VD'];
-        const salesPerLocation = {};
+        const salesPerCity = {};
 
         if (Array.isArray(sales)) {
             sales.forEach(sale => {
                 if (!(sale.Line.length && validTypes.includes(sale.InvoiceType))) return;
 
-                if (salesPerLocation[sale.ShipTo.Address.City]) {
-                    salesPerLocation[sale.ShipTo.Address.City].quantity += 1;
-                    salesPerLocation[sale.ShipTo.Address.City].netTotal += parseFloat(
+                if (salesPerCity[sale.ShipTo.Address.City]) {
+                    salesPerCity[sale.ShipTo.Address.City].quantity += 1;
+                    salesPerCity[sale.ShipTo.Address.City].netTotal += parseFloat(
                         sale.DocumentTotals.NetTotal,
                     );
                 } else {
-                    salesPerLocation[sale.ShipTo.Address.City] = {
+                    salesPerCity[sale.ShipTo.Address.City] = {
                         quantity: 1,
                         netTotal: parseFloat(sale.DocumentTotals.NetTotal),
                     };
                 }
             });
         } else {
-            salesPerLocation[sales.ShipTo.Address.City] = {
+            salesPerCity[sales.ShipTo.Address.City] = {
                 quantity: 1,
                 netTotal: parseFloat(sales.DocumentTotals.NetTotal),
             };
         }
 
-        res.json(salesPerLocation);
+        res.json(salesPerCity);
     });
 }
