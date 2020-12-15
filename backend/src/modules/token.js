004b7d519121cf21e1902132248164484f5c7a41
Nuno Cardoso <up201706162@fe.up.pt>
2020-12-15 21:55:07 +0000

    Multiple accounts

--- backend/src/modules/token.js
+++ backend/src/modules/token.js
@@ -4,27 +4,31 @@ module.exports = (server, db, accounts) => {
   server.get("/token", (req, res) => {
     let username = 'username' in req.query ? req.query['username'] : null;
     let password = 'password' in req.query ? req.query['password'] : null;
-    if (JSON.stringify(accounts.User.username) == username && JSON.stringify(accounts.User.password) == password) {
-      const options = {
-        method: "POST",
-        url: "https://identity.primaverabss.com/connect/token",
-        headers: {
-          "Content-Type": "multipart/form-data",
-          "Access-Control-Allow-Origin": "*",
-        },
-        formData: {
-          client_id: "FEUPSINF",
-          client_secret: "23f8ba55-b745-4857-9f6f-d7f53391750b",
-          scope: "application",
-          grant_type: "client_credentials",
-        },
-      };
-
-      request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        res.header("Access-Control-Allow-Origin", "*");
-        res.json(body);
-      });
-    }
+    accounts.User.forEach(element => {
+      let user = element.username;
+      let pass = element.password;
+      if (JSON.stringify(user) == username && JSON.stringify(pass) == password) {
+        const options = {
+          method: "POST",
+          url: "https://identity.primaverabss.com/connect/token",
+          headers: {
+            "Content-Type": "multipart/form-data",
+            "Access-Control-Allow-Origin": "*",
+          },
+          formData: {
+            client_id: "FEUPSINF",
+            client_secret: "23f8ba55-b745-4857-9f6f-d7f53391750b",
+            scope: "application",
+            grant_type: "client_credentials",
+          },
+        };
+  
+        request(options, function (error, response, body) {
+          if (error) throw new Error(error);
+          res.header("Access-Control-Allow-Origin", "*");
+          res.json(body);
+        });
+      }
+    }); 
   });
 };
